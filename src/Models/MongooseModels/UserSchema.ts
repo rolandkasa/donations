b3abb30bbd267796254acf988b5dc188524f60bd
Roland Kasa <roland.kasa@softvision.ro>
2019-09-10 10:40:38 +0300

    Update: user creation and authentication.

---
+++ src/Models/MongooseModels/UserSchema.ts
@@ -0,0 +1,43 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+interface UserInterface{
+    username: String,
+    email: String,
+    password: string,
+    registeredAt: Date,
+}
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: 'Add username.'
+    },
+    email: {
+        type: String,
+        required: 'Add email'
+    },
+    password: {
+        type: String,
+        required: 'Add password'            
+    },
+    registeredAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    toObject: {
+      transform: function (doc, ret) {
+        delete ret.__V;
+      }
+    },
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.__V;
+      }
+    }
+});
+
+const User = mongoose.model<UserInterface>('User', UserSchema)
+export default User
--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,64 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { BaseRoutes } from "./Routes/BaseRoutes";
 import MongoHelper from './Utils/MongoHelper'
+import * as connectMongo from 'connect-mongo';
+import * as ExpressSession from 'express-session'
+import * as cookieParser from 'cookie-parser'
+import * as mongoose from 'mongoose'
+import * as passport from 'passport'
+const cookieSession = require('cookie-session')
+
 
 class App {
 
     public app: express.Application;
     public baseRoutes: BaseRoutes = new BaseRoutes();
+    public mongoHelper: MongoHelper = new MongoHelper();
 
     constructor() {
         this.app = express();
         this.config();   
-        this.baseRoutes.routes(this.app)
     }
 
     private config(): void{
-        this.connectToDB();
+        this.connectToDB().then(() => {
+            try{
+                this.app.use(express.static('public'));
+                
+                // support application/json type post data
+                this.app.use(bodyParser.json());
+                //support application/x-www-form-urlencoded post data
+                this.app.use(bodyParser.urlencoded({ extended: false }));
 
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));   
+                this.app.use(cookieParser())
+                this.app.use(cookieSession({
+                    name: 'session',
+                    keys: ['session1', 'session2']
+                }))
+                
+                const MongoStore = connectMongo(ExpressSession);
+
+                this.app.use(ExpressSession({
+                    secret: 'donations',
+                    resave: true,
+                    saveUninitialized: true,
+                    unset: 'destroy',
+                    cookie: { secure: true,
+                        maxAge:  6*60*60*1000 },
+                    store: new MongoStore({ mongooseConnection: this.mongoHelper.getDB() })
+                }));
+                this.app.use(passport.initialize());
+                this.app.use(passport.session());
+
+                this.baseRoutes.routes(this.app)
+            }catch(e){
+                console.log(e)
+            }
+        });
     }
 
-    private connectToDB(): void{
-        MongoHelper.connect('mongodb://localhost:27017/donations')
+    private connectToDB(): Promise<void>{
+        return this.mongoHelper.connect('mongodb://localhost:27017/donations')
     }
 }
 
