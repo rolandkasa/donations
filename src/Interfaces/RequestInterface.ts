fee19877ddebcfa986ec1d49e79245dc2d196539
Roland Kasa <roland.kasa@softvision.ro>
2019-09-11 13:53:42 +0300

    Update: added logout method to request interface.

--- src/Interfaces/RequestInterface.ts
+++ src/Interfaces/RequestInterface.ts
@@ -4,5 +4,6 @@ import User, {UserInterface} from '../Models/MongooseModels/UserSchema'
 export default interface RequestInterface extends Request{
     user: UserInterface;
     isAuthenticated(): boolean;
-    logIn(user: UserInterface, cb: Function): void;
+    logIn(user: UserInterface, cb: Function): Promise<void>;
+    logout(): Promise<void>
 }
