464137cf42cda8326d14b39c60c683f0fa054506
Roland Kasa <roland.kasa@softvision.ro>
2019-09-11 10:29:59 +0300

    Update: added comments to UserController.

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -3,9 +3,14 @@ import FormatResponse from '../Utils/FormatResponse';
 import User from '../Models/MongooseModels/UserSchema'
 import {generate} from 'password-hash';
 import passport from '../UserAuthenticators/LocalStrategy'
+import RequestInterface from '../Interfaces/RequestInterface'
 
 export class UserController{
-    public async registerUser(req: Request, res: Response){
+    /**
+     * POST /user
+     * Register new user.
+     */
+    public async registerUser(req: Request, res: Response): Promise<Response> {
         let user = new User(req.body)
 
         try{
@@ -17,7 +22,11 @@ export class UserController{
         }
     }
 
-    public async getUsers(req, res){
+    /**
+     * GET /user
+     * Get all users.
+     */
+    public async getUsers(req: RequestInterface, res: Response): Promise<Response>{
         try{
             return res.json(FormatResponse.transform(await User.find(), 200))
         }catch(error){
@@ -26,10 +35,10 @@ export class UserController{
     }
 
     /**
- * POST /login
- * Sign in using email and password.
- */
-    public postLogin (req, res: Response,next) {
+     * POST /login
+     * Sign in using email and password.
+     */
+    public postLogin (req: RequestInterface, res: Response,next ){
         passport.authenticate("local",  { failureRedirect: '/login' }, (err: Error, user, info) => {
             if (err) { 
                 return res.json(FormatResponse.transform(err,500))
@@ -41,7 +50,8 @@ export class UserController{
                 if (err) { 
                     return res.json(FormatResponse.transform(err,500))
                 }
-                return res.send(FormatResponse.transform(req.session,200))
+
+                return res.send(FormatResponse.transform(user,200))
             });
         })(req, res, next);
     };
